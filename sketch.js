ef1d17ab8b144436ee76719f84db6592269d340b
Dippinlow <euanknight@icloud.com>
2023-05-16 12:04:04 +0100

    Adjusted the canvas to fit the screen Changed the background colour to Ash gray

--- sketch.js
+++ sketch.js
@@ -1,14 +1,14 @@
 function setup() {
-  createCanvas(600, 600);
+  createCanvas(windowWidth, windowHeight);
   noLoop();
 }
 
 function draw() {
-  background(220);
+  background(176, 196, 177);
 
   for(let i = 0; i  <width; i++){
     for(let j = 0; j < height; j++){
-      if(floor(random(2)) == 0){
+      if(floor(random(3)) == 0){
         stroke(255, 10);
       }else{
         stroke(0, 10);
