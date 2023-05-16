34e86bfb389e617afec28d6c564560f5a4f30f63
Dippinlow <euanknight@icloud.com>
2023-05-16 12:25:45 +0100

    Pre computed the grain layer added a circle and a spinining line

--- sketch.js
+++ sketch.js
@@ -1,20 +1,37 @@
+let grainLayer;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  noLoop();
+  //noLoop();
+  fill(74, 87, 89);
+  grainLayer = createGraphics(width, height);
+
+  for(let i = 0; i  < width; i++){
+    for(let j = 0; j < height; j++){
+      if(floor(random(3)) == 0){
+        grainLayer.stroke(255, 10);
+      }else{
+        grainLayer.stroke(0, 10);
+      }
+
+      grainLayer.point(i, j);
+    }
+  }
 }
 
 function draw() {
   background(176, 196, 177);
 
-  for(let i = 0; i  <width; i++){
-    for(let j = 0; j < height; j++){
-      if(floor(random(3)) == 0){
-        stroke(255, 10);
-      }else{
-        stroke(0, 10);
-      }
+  stroke(0);
+  push();
+  translate(width/2, height/2);
+  rotate(frameCount / 360 * TWO_PI);
+  line(-width/2, 0, width/2, 0);
 
-      point(i, j);
-    }
-  }
+  pop();
+  
+  circle(width/2, height/2, width/2);
+
+
+  image(grainLayer, 0, 0);
 }
